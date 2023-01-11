da0d0c4a044ce53d79f3ebf9f69a1559816827fc
antony waweru <anto.waweru2030@gmail.com>
2023-01-11 18:47:28 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-// Code here
+//fetching beer data and rendering elements to the DOM
+url='http://localhost:3000/beers'
+ 
+ function getBeers(beerID=1){
+  fetch(`${url}/${beerID}`)
+  .then(res => res.json())
+  .then(data => {
+document.querySelector('#beer-name').textContent = data.name
+document.querySelector('#beer-image').src = data.image_url
+//document.querySelector('beer-name').dataset.id=data.id
+document.querySelector('#beer-description').textContent=data.description
+renderBeerReview(data.reviews)
+
+}
+)}
+//Updating reviews
+function renderBeerReview(review){
+document.querySelector('#review-list').innerHTML = ''
+for(let i=0; i<review.length; i++){
+  let li = document.createElement('li')
+  li.innerHTML=review
+  let list = document.querySelector('#review-list')
+  list.appendChild(li)
+};
+}
+//updating menu
+document.querySelector('#beer-list').innerHTML = ''
+function replaceBeer(beers){
+  for(const element in beers){
+    let beerID=beers[element].beerID
+    let li=document.createElement('li')
+    li.addEventListener('click', (e)=>{
+      e.prevemtDefault()
+      displayBeer(beerID)
+    })
+
+    li.textContent = beers[element].name
+    document.querySelector('#beer-list').appendChild(li)
+  }
+}
+
+
+
+getBeers()
+
+  
---
+++ src/mewfile.js
@@ -0,0 +1,412 @@
+const beerReviewForm = document.getElementById('review-form');
+const beerReviewText = document.getElementById('review');
+
+function beerDisplay(beer){
+
+    // changing of our beer descriptions
+    const beerDescriptionForm= document.getElementById('description-form');
+    const beerEditDescription = document.getElementById('description');
+    //  beerEditedDescriptionForm.reset();
+
+    // removing place-holders for customer reviews
+    const beerReviewList = document.getElementById('review-list');
+    while (beerReviewList.firstElementChild){
+        beerReviewList.removeChild(beerReviewList.lastElementChild)
+    };
+
+    // Updating beer Name, beer Image, beer description
+    // displaying reviews to each specific beer
+
+    beerName.textContent = beer.name,             
+    beerImage.src = beer.image_url,                     
+    beerDescription.textContent = beer.description,     
+    beerEditDescription.value = beer.description    
+
+    // code to display beer reviews of each individual beer
+
+    for(let review of beer.reviews){
+        let beerReview = document.createElement('li');
+        beerReview.textContent = review;
+        beerReviewList.appendChild(beerReview);
+    }
+
+    // code to update beer description
+        beerDescriptionForm.addEventListener('submit',updateDescription);
+
+   
+    function updateDescription(event){
+        
+        event.preventDefault();        
+        beer.description = beerEditDescription.value;
+      updateBeer(beer)
+    };
+// this code will add reviews  
+    beerReviewForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+ function beerDisplay(beer) {
+
+     // declare variables to enable editing of our beer descriptions
+     const beerDescriptionForm = document.getElementById('description-form');
+     const beerEditDescription = document.getElementById('description');
+     //  beerEditedDescriptionForm.reset();
+
+     // code to remove place-holder for customer reviews
+     const beerReviewList = document.getElementById('review-list');
+     while (beerReviewList.firstElementChild) {
+         beerReviewList.removeChild(beerReviewList.lastElementChild)
+     };
+
+
+     
+        if(beerReviewText.value !== ''){
+
+            beer.reviews.push(beerReviewText.value)
+            updateBeer(beer)
+        } else{
+            alert('Please add Review!')
+        }
+    };
+});
+
+
+// function using Patch Method which updates beer changes(beer description and add beer reviews)
+function updateBeer(beer){
+    //use fetch to pick beer id to be updated
+    fetch(`http://localhost:3000/beers/${beer.id}`,
+        {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(beer)
+        })
+        .then(response => response.json())
+        .then(data => beerDisplay(data))
+
+};
+
+// extra practice -  create Beer name, description, image Url and its reviews
+//using beer = null to show there exist a variable
+
+function fetchData(beer=null){
+    let baseURL = 'http://localhost:3000/beers/'
+    //use class to  create a promise using the promise constructor
+    return new Promise((resolve,) => {
+        let url = beer == null ? baseURL : `${baseURL + beer}`
+        fetch(url)
+        .then(response => response.json())
+        .then(data => resolve(data))
+
+        })
+    };
+
+
+    //function to display navigation on the left side
+function navDisplay(beers){
+
+//populate the left side bar withe beer names
+    const navBeerList = document.querySelector('#beer-list');
+    while (navBeerList.firstElementChild){
+        //remove last child element will remove 
+        // <li>Beer Name</li>
+        //   <li>Another Beer Name</li>
+        //   <li>And Another Beer Name</li>
+        navBeerList.removeChild(navBeerList.lastElementChild)
+    };
+
+    beers.forEach(beer => {
+        const navElement = document.createElement('li');
+        navElement.textContent = beer.name;
+        navElement.setAttribute('index', beer.id);
+        navBeerList.append(navElement)
+
+// add event listener to beernames onCLick
+        navElement.addEventListener('click', (event)=> {
+           //when clicked fetches beer dat using its id value
+            fetchData(event.target.getAttribute('index'))
+            .then(beer => {
+                beerDisplay(beer);
+            });
+        });
+    });
+
+
+};
+
+function initializeFlataBeer(){
+    //use asynchronous javascript
+    //displays the navbar
+    fetchData()
+    .then(beers => navDisplay(beers))
+//then display the renderpage
+    fetchData(1)
+    .then(beers => beerDisplay(beers))
+
+};
+
+initializeFlataBeer()
+
+     // code to display beer Name, beer Image, beer description, edit the description and add reviews
+     // add reviews to each specific beer
+
+     beerName.textContent = beer.name,
+         beerImage.src = beer.image_url,
+         beerDescription.textContent = beer.description,
+         beerEditDescription.value = beer.description
+
+     // code to display beer reviews of each individual beer
+
+     for (let review of beer.reviews) {
+         let beerReview = document.createElement('li');
+         beerReview.textContent = review;
+         beerReviewList.appendChild(beerReview);
+     }
+
+     // code to update beer description
+     beerDescriptionForm.addEventListener('submit', updateDescription);
+
+     // function call to update description
+     function updateDescription(event) {
+         // e.preventDefault() prevents page from reloading
+         event.preventDefault();
+         beer.description = beerEditDescription.value;
+         updateBeer(beer)
+     };
+     // this code will add reviews  
+     beerReviewForm.addEventListener('submit', (event) => {
+         event.preventDefault();
+
+         // conditional that will not allow users to add empty reviews
+         if (beerReviewText.value !== '') {
+
+             beer.reviews.push(beerReviewText.value)
+             updateBeer(beer)
+         } else {
+             alert('Please add Review!')
+         }
+     });
+ };
+
+
+ // function using Patch Method which updates beer changes(beer description and add beer reviews)
+ function updateBeer(beer) {
+     //use fetch to pick beer id to be updated
+     fetch(`http://localhost:3000/beers/${beer.id}`, {
+             method: 'PATCH',
+             headers: { 'Content-Type': 'application/json' },
+             body: JSON.stringify(beer)
+         })
+         .then(response => response.json())
+         .then(data => beerDisplay(data))
+
+ };
+
+ // extra practice could include A POST method to create Beer name, description, image Url and its reviews
+
+
+ //using method GET to fetch beer data
+ //using beer = null to show there exist a variable
+ function fetchData(beer = null) {
+     let baseURL = 'http://localhost:3000/beers/'
+         //use class to  create a promise using the promise constructor
+     return new Promise((resolve, ) => {
+         let url = beer == null ? baseURL : `${baseURL + beer}`
+         fetch(url)
+             .then(response => response.json())
+             .then(data => resolve(data))
+
+     })
+ };
+
+
+ //function to display navigation on the left side
+ function navDisplay(beers) {
+
+     //populate the left side bar withe beer names
+     const navBeerList = document.querySelector('#beer-list');
+     while (navBeerList.firstElementChild) {
+         //remove last child element will remove 
+         // <li>Beer Name</li>
+         //   <li>Another Beer Name</li>
+         //   <li>And Another Beer Name</li>
+         navBeerList.removeChild(navBeerList.lastElementChild)
+     };
+
+     beers.forEach(beer => {
+         const navElement = document.createElement('li');
+         navElement.textContent = beer.name;
+         navElement.setAttribute('index', beer.id);
+         navBeerList.append(navElement)
+
+         // add event listener to beernames onCLick
+         navElement.addEventListener('click', (event) => {
+             //when clicked fetches beer dat using its id value
+             fetchData(event.target.getAttribute('index'))
+                 .then(beer => {
+                     beerDisplay(beer);
+                 });
+         });
+     });
+
+
+ };
+
+ function initializeFlataBeer() {
+     //use asynchronous javascript
+     //displays the navbar
+     fetchData()
+         .then(beers => navDisplay(beers))
+         //then display the renderpage
+     fetchData(1)
+         .then(beers => beerDisplay(beers))
+
+ };
+ initializeFlataBeer()ument.getElementById('review-form');
+ const beerReviewText = document.getElementById('review');
+
+function beerDisplay(beer){
+
+    // declare variables to enable editing of our beer descriptions
+    const beerDescriptionForm= document.getElementById('description-form');
+    const beerEditDescription = document.getElementById('description');
+    //  beerEditedDescriptionForm.reset();
+
+    // code to remove place-holder for customer reviews
+    const beerReviewList = document.getElementById('review-list');
+    while (beerReviewList.firstElementChild){
+        beerReviewList.removeChild(beerReviewList.lastElementChild)
+    };
+
+
+
+    // code to display beer Name, beer Image, beer description, edit the description and add reviews
+    // add reviews to each specific beer
+
+    beerName.textContent = beer.name,             
+    beerImage.src = beer.image_url,                     
+    beerDescription.textContent = beer.description,     
+    beerEditDescription.value = beer.description    
+
+    // code to display beer reviews of each individual beer
+
+    for(let review of beer.reviews){
+        let beerReview = document.createElement('li');
+        beerReview.textContent = review;
+        beerReviewList.appendChild(beerReview);
+    }
+
+    // code to update beer description
+        beerDescriptionForm.addEventListener('submit',updateDescription);
+
+    // function call to update description
+    function updateDescription(event){
+        // e.preventDefault() prevents page from reloading
+        event.preventDefault();        
+        beer.description = beerEditDescription.value;
+      updateBeer(beer)
+    };
+// this code will add reviews  
+    beerReviewForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+ function beerDisplay(beer) {
+
+     // declare variables to enable editing of our beer descriptions
+     const beerDescriptionForm = document.getElementById('description-form');
+     const beerEditDescription = document.getElementById('description');
+     //  beerEditedDescriptionForm.reset();
+
+     // code to remove place-holder for customer reviews
+     const beerReviewList = document.getElementById('review-list');
+     while (beerReviewList.firstElementChild) {
+         beerReviewList.removeChild(beerReviewList.lastElementChild)
+     };
+
+
+         // conditional that will not allow users to add empty reviews
+        if(beerReviewText.value !== ''){
+
+            beer.reviews.push(beerReviewText.value)
+            updateBeer(beer)
+        } else{
+            alert('Please add Review!')
+        }
+    });
+};
+
+
+// function using Patch Method which updates beer changes(beer description and add beer reviews)
+function updateBeer(beer){
+    //use fetch to pick beer id to be updated
+    fetch(`http://localhost:3000/beers/${beer.id}`,
+        {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(beer)
+        })
+        .then(response => response.json())
+        .then(data => beerDisplay(data))
+
+};
+
+// extra practice could include A POST method to create Beer name, description, image Url and its reviews
+
+
+//using method GET to fetch beer data
+//using beer = null to show there exist a variable
+function fetchData(beer=null){
+    let baseURL = 'http://localhost:3000/beers/'
+    //use class to  create a promise using the promise constructor
+    return new Promise((resolve,) => {
+        let url = beer == null ? baseURL : `${baseURL + beer}`
+        fetch(url)
+        .then(response => response.json())
+        .then(data => resolve(data))
+
+        })
+    };
+
+
+    //function to display navigation on the left side
+function navDisplay(beers){
+
+//populate the left side bar withe beer names
+    const navBeerList = document.querySelector('#beer-list');
+    while (navBeerList.firstElementChild){
+        //remove last child element will remove 
+        // <li>Beer Name</li>
+        //   <li>Another Beer Name</li>
+        //   <li>And Another Beer Name</li>
+        navBeerList.removeChild(navBeerList.lastElementChild)
+    };
+
+    beers.forEach(beer => {
+        const navElement = document.createElement('li');
+        navElement.textContent = beer.name;
+        navElement.setAttribute('index', beer.id);
+        navBeerList.append(navElement)
+
+
+        navElement.addEventListener('click', (event)=> {
+           //when clicked fetches beer dat using its id value
+            fetchData(event.target.getAttribute('index'))
+            .then(beer => {
+                beerDisplay(beer);
+            });
+        });
+    });
+
+
+};
+
+function initializeFlataBeer(){
+    //use asynchronous javascript
+    //displays the navbar
+    fetchData()
+    .then(beers => navDisplay(beers))
+//then display the renderpage
+    fetchData(1)
+    .then(beers => beerDisplay(beers))
+
+};
+
+initializeFlataBeer()
+
+     
